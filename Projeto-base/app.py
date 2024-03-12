d4ba01462e368ff2b9f89d6c57698aa67ee6198b
Luka <lukadbarth@hotmail.com>
2024-03-12 17:29:30 -0300

    We refactored the code by creating a property inside 'restaurante.py' to add a method that displays the menu to the user

--- Projeto-base/app.py
+++ Projeto-base/app.py
@@ -7,9 +7,11 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5, 'Grande')
 prato_paozinho = Prato('Pãozinho', 2, 'O melhor pão da cidade')
 
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
