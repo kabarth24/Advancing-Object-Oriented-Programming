00dab70ab31fe21b041418280ed13b7e2fec829b
Luka <lukadbarth@hotmail.com>
2024-03-13 15:19:27 -0300

    I added a dessert child class as specified in the 'Hands on' step

--- Projeto-base/app.py
+++ Projeto-base/app.py
@@ -1,6 +1,7 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 
@@ -8,9 +9,11 @@ bebida_suco = Bebida('Suco de Melancia', 5, 'Grande')
 bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Pãozinho', 2, 'O melhor pão da cidade')
 prato_paozinho.aplicar_desconto()
+sobremesa1 = Sobremesa('Sorvete de Brownie', 10, 'Doce', 'Médio')
 
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(sobremesa1)
 
 def main():
     restaurante_praca.exibir_cardapio
